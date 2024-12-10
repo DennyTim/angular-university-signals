7a28833e722ca522328292a68d88351ca7570808
dety0819 <denys.tymokhin@netcracker.com>
2024-12-10 20:29:17 +0200

    ------initial commit------

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,36 @@
-import {Component, inject} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
-import {MatListItem, MatNavList} from "@angular/material/list";
-import {MatIcon} from "@angular/material/icon";
-import {MatToolbar} from "@angular/material/toolbar";
-import {MatIconButton} from "@angular/material/button";
-import {LoadingIndicatorComponent} from "./loading/loading.component";
-import {MessagesComponent} from "./messages/messages.component";
-
+import { Component } from "@angular/core";
+import { MatIconButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import {
+  MatListItem,
+  MatNavList
+} from "@angular/material/list";
+import {
+  MatSidenav,
+  MatSidenavContainer
+} from "@angular/material/sidenav";
+import { MatToolbar } from "@angular/material/toolbar";
+import {
+  RouterLink,
+  RouterOutlet
+} from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [
-    RouterOutlet, MatSidenavContainer, MatSidenav, MatNavList, MatListItem, MatIcon, RouterLink, MatToolbar,
-    MatIconButton, LoadingIndicatorComponent, MessagesComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    RouterOutlet,
+    MatSidenavContainer,
+    MatSidenav,
+    MatNavList,
+    MatListItem,
+    MatIcon,
+    RouterLink,
+    MatToolbar,
+    MatIconButton
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss"
 })
 export class AppComponent {
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,83 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import {
+  Component,
+  computed,
+  effect,
+  EffectRef,
+  inject,
+  Injector,
+  signal
+} from "@angular/core";
+import {
+  MatTab,
+  MatTabGroup
+} from "@angular/material/tabs";
 
 @Component({
-  selector: 'home',
+  selector: "home",
   standalone: true,
   imports: [
     MatTabGroup,
-    MatTab,
-    CoursesCardListComponent
+    MatTab
   ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.scss"
 })
 export class HomeComponent {
+  counter = signal(0);
 
+  tenXCounter = computed(() => {
+    const val = this.counter();
+    return val * 10;
+  });
+
+  hundredXCounter = computed(() => {
+    const val = this.tenXCounter();
+
+    return val * 10;
+  });
+
+  effectRef: EffectRef | null = null;
+  injector = inject(Injector);
+
+  constructor() {
+
+    this.effectRef = effect((onCleanup) => {
+
+      const counter = this.counter();
+
+      const timeout = setTimeout(() => {
+        window.console.log(`counter value: ${counter}`);
+      }, 1000);
+
+      onCleanup(() => {
+        console.log(`Calling clean up`);
+        clearTimeout(timeout);
+      });
+    });
+
+    // TODO: How to fix ERROR RuntimeError: NG0600: Writing to signals is not allowed
+    //  in a `computed` or an `effect` by default.
+    /*** Solution: { allowSignalWrites: true } */
+    // effect(() => {
+    //   window.console.log(`counter value: ${this.counter()}`);
+    //   this.append();
+    // }, { allowSignalWrites: true });
+
+    // TODO: How to fix ERROR RuntimeError: NG0203: effect()
+    //  can only be used within an injection context
+    /*** Solution: { injector: this.injector } */
+    // afterNextRender(() => {
+    //   effect(() => {
+    //     window.console.log(`counter value: ${this.counter()}`);
+    //   }, { injector: this.injector });
+    // });
+  }
+
+  public append(): void {
+    this.counter.update(val => val + 1);
+  }
+
+  public cleanup(): void {
+    this.effectRef?.destroy();
+  }
 }
